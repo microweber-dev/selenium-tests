2b3db51d5693891921147678ed3fc4fb22181905
Peter Ivanov <peter@microweber.com>
2018-06-24 14:01:42 +0300

    update

---
+++ tests/mw_test/Object Repository/Page_Gallery/a_Text Page.rs
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Text Page</name>
+   <tag></tag>
+   <elementGuidId>c091a34f-cc12-418a-9925-001c0b62e498</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>itemprop</name>
+      <type>Main</type>
+      <value>url</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-item-id</name>
+      <type>Main</type>
+      <value>11</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>menu_element_link   first-child child-0 menu-item-id-11 menu-item-parent-1 depth-0  </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>http://slave01.ci.microweber.com:8080/test-text-page</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Text Page</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;module-menu-20&quot;)/div[@class=&quot;module-navigation module-navigation-default&quot;]/ul[@class=&quot;nav menu_1  menu-root menu-item-id-6 menu-item-parent-1&quot;]/li[@class=&quot;menu_element    depth-0&quot;]/a[@class=&quot;menu_element_link   first-child child-0 menu-item-id-11 menu-item-parent-1 depth-0&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
