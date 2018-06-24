8d2059414215779835a4bdbdf38156f1550533bb
Peter Ivanov <peter@microweber.com>
2018-06-24 14:13:34 +0300

    update

---
+++ tests/mw_test/Test Suites/browse_test.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>browse_test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-06-24T14:13:13</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>dd7082e8-f5ff-47a7-beed-b95f346b75bd</testSuiteGuid>
+   <testCaseLink>
+      <guid>dff60fb0-0807-44ad-a307-03627ecc22b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/browse test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
